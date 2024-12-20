6b292a9c83aa0b59795c94d05fbd281d7840546e
dicky ardiar <dickyardiar1@gmail.com>
2024-12-20 14:08:21 +0700

    adding unit test in library

--- src/types/userStatrtFetchResult.ts
+++ src/types/userStatrtFetchResult.ts
@@ -3,3 +3,10 @@ export interface useFethResult<T> {
   loading: boolean;
   error: string | null;
 }
+
+export interface useFetchIfProps<T> {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+  startFetcing: boolean;
+}
