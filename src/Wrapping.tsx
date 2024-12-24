42ae5c02f35d7334117bad003b28d292ce84db67
dicky ardiar <dickyardiar1@gmail.com>
2024-12-24 14:55:05 +0700

    revamp more folder

--- src/Wrapping.tsx
+++ src/Wrapping.tsx
@@ -1,62 +1,15 @@
-import React, { useContext, useEffect, useState } from "react"
-import { WrappingProps } from "./types/Wrapping"
-import { apiContextProps } from "./types/apiContextProps"
-import { useFetch } from "./startFetching/useFetch";
-import { useFetchIf } from "./startFetching/useFetchIf";
+import React, { ReactNode } from 'react';
+import { ApiProvider } from "./context/apiContext";
+import { WrappingComponentProps } from "./types/Wrapping";
 
-const ApiContainer = React.createContext<apiContextProps | undefined>(undefined);
-
-export const useApiContainer = () => {
-  const context  = useContext(ApiContainer);
-  if (!context) {
-    throw new Error('useApiContext must be used within a ApiProvider')
-  }
-  return context
-}
-
-const WrappingComponent: React.FC<WrappingProps> = ({ children }) => {
-
-  const [data, setData] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const startFetching = async (urlRequest: string, methodRequest: string | undefined, bodyRequest: any) => {
-    const { response, loading, error } = useFetch(
-      urlRequest,
-      methodRequest,
-      bodyRequest
-    );
-    setData(response);
-    setLoading(loading);
-    setError(error);
-  }
-
-  const startFetchingIf = async (urlRequest: string, methodRequest: string | undefined, bodyRequest: any, startFetchingReq?: boolean) => {
-    const { response, loading, error, startFetching: startFetch } = useFetchIf(
-      urlRequest,
-      methodRequest,
-      bodyRequest,
-      !!startFetchingReq
-    );
-    if (startFetch) {
-      setData(response);
-      setLoading(loading);
-      setError(error);
-    }
-  };
-
+const WrappingComponent = ({ children }: WrappingComponentProps) => {
   return (
     <div className="wrapping-component">
-      <ApiContainer.Provider value={{ startFetching, startFetchingIf}}>
+      <ApiProvider>
         {children}
-      </ApiContainer.Provider>
-
-      {loading && <div>Loading...</div>}
-      {error && <div>Error: {error}</div>}
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-      {children}
+      </ApiProvider>
     </div>
-  )
+  );
 }
 
-export default WrappingComponent
+export default WrappingComponent;
